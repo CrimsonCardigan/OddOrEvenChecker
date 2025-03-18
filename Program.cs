4f44a9acff5311b735283049711479dbd2ecb3fb
CrimsonCardigan <CrimsonCardigan@users.noreply.github.com>
2025-03-18 11:33:45 -0500

    UwU

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿while (true) {
+    Console.WriteLine("Hello. Input a number and I can tell you if it is odd or even.");
+
+    var GivenUserNumber = Console.ReadLine();
+    int UserNumber = int.Parse(GivenUserNumber);
+
+    if (UserNumber % 2 == 0)        //if the number devided by 2 = 0, (it is even)
+    {
+        Console.WriteLine("The Number: " + UserNumber + " is even ");
+    }
+    else
+    {
+        Console.WriteLine("The Number: " + UserNumber + " is odd ");
+    }
+
+    Console.WriteLine();
+}

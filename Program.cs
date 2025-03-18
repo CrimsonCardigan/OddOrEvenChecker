54f2bc689717d9f659b5e1b512d6d0f34a6a586d
CrimsonCardigan <CrimsonCardigan@users.noreply.github.com>
2025-03-18 15:09:40 -0500

    Color, warning, loop, and exit.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿
-
-while (true) 
+﻿while (true) 
 
 {
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine("Hello! Input a number and I can tell you if it is odd or even.");
 
     string GivenUserNumber = Console.ReadLine()!;
+
+if (GivenUserNumber.ToLower() == "exit")
+    {
+        break; // Exit the loop if the user types 'exit'
+    }
 
     int UserNumber;
 
@@ -21,14 +24,14 @@ while (true)
     {
         if (UserNumber % 2 == 0)        //if the number devided by 2 = 0, (it is even)
             {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("The Number: " + UserNumber + " is even ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("The Number: " + UserNumber + " is even ");
             }
             else
-                {
+            {
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine("The Number: " + UserNumber + " is odd ");
-                }
+            }
                 Console.WriteLine();
     }
 }

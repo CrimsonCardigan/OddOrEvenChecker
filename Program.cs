c33a466bedd989a7f33cf5123455fb8f058a9788
CrimsonCardigan <CrimsonCardigan@users.noreply.github.com>
2025-03-18 14:52:22 -0500

    Color, warning, and loop

--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
-﻿while (true) {
+﻿
+
+while (true) 
+
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine("Hello! Input a number and I can tell you if it is odd or even.");
 
-    var GivenUserNumber = Console.ReadLine();
-    int UserNumber = int.Parse(GivenUserNumber);
+    string GivenUserNumber = Console.ReadLine()!;
 
-    if (UserNumber % 2 == 0)        //if the number devided by 2 = 0, (it is even)
+    int UserNumber;
+
+    bool isValid = int.TryParse(GivenUserNumber, out UserNumber);
+
+      if (!isValid)
     {
-        Console.WriteLine("The Number: " + UserNumber + " is even ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid input. Please enter a valid whole number.");
     }
     else
     {
-        Console.WriteLine("The Number: " + UserNumber + " is odd ");
+        if (UserNumber % 2 == 0)        //if the number devided by 2 = 0, (it is even)
+            {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("The Number: " + UserNumber + " is even ");
+            }
+            else
+                {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("The Number: " + UserNumber + " is odd ");
+                }
+                Console.WriteLine();
     }
-
-    Console.WriteLine();
 }
